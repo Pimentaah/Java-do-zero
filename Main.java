1ea1191c7dd45d145bbb276b8d908036f828143b
Pedro Pimenta <pedropimentaht@gmail.com>
2026-03-17 22:51:56 -0300

    Testegit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String senha = "";
+
+while(!senha.equals("123")){
+    // pedir senha
+}
+    }
+}
+
---
+++ Teste.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+class Dados {
+
+    String nome;
+    int idade;
+    String graduado;
+    String formacao;
+    String experiencia;
+
+}
+public class Teste {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        Dados pessoa1 = new Dados();
+
+        System.out.println("Digite seu nome inteiro: ");
+        pessoa1.nome = leitor.nextLine();
+
+        System.out.println("Você tem ensino médio? S/N:  ");
+        pessoa1.graduado = leitor.nextLine();
+        
+        if (pessoa1.graduado.equalsIgnoreCase("Sim")) {
+            
+            System.out.println("Parabéns pela graduação!");
+
+        } else { 
+            System.out.println("Sinto muito, mas precisamos que tenha Ensino médio completo");
+
+            return;
+        }
+        
+        System.out.println("Qual é seu nivel de escolaridade?");
+        pessoa1.formacao = leitor.nextLine();
+
+        if (pessoa1.formacao.equalsIgnoreCase("Ensino médio incompleto")) {
+            System.out.print("Você precisa ter Ensino médio completo para prosseguir!");
+            pessoa1.formacao = leitor.nextLine();
+        }
+
+        else if (pessoa1.formacao.equalsIgnoreCase("Ensino superior cursando")) {
+            System.out.println("Qual curso está matriculado?");
+            pessoa1.formacao = leitor.nextLine();
+            System.out.println("Que legal, boa sorte na graduação!");
+        }
+
+        else if (pessoa1.formacao.equalsIgnoreCase("Ensino superior completo")) {
+            System.out.print("Qual curso você se formou?");
+            pessoa1.formacao = leitor.nextLine();
+        }
+
+        System.out.println("Tem experiência na área de Tecnológica?");
+        pessoa1.experiencia = leitor.nextLine();
+
+        if (pessoa1.experiencia.equalsIgnoreCase("Sim")) {
+            System.out.println("Fale um pouco de como foi sua experiência: ");
+            pessoa1.experiencia = leitor.nextLine();
+        } else {
+
+        }
+         
+            
+        System.out.println("Tem alguma experiência profissional? ");
+        pessoa1.experiencia = leitor.nextLine();
+
+        if (pessoa1.experiencia.equalsIgnoreCase("Sim ")) {
+            System.out.println("Descreva um pouco sobre suas experiências: ");
+            pessoa1.experiencia = leitor.nextLine();
+        } else {
+            System.out.println("Siga para a próxima etapa!");
+        }
+        
+        asd
+    }
+}
+
+
+
+
